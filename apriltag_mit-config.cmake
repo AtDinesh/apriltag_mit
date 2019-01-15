e2f5446612ee08ed14b7de8fa09e75d67272eec8
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2019-01-15 16:30:04 +0100

    [WIP] working on cmake. . .

---
+++ apriltag_mit-config.cmake
@@ -0,0 +1,5 @@
+# apriltag_mit-config.cmake - package configuration file
+
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME}.cmake)
+
